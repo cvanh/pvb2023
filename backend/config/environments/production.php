b23eb5444b3e8853808f1596d4e558160b4ccba1
constantijn van hartesveldt <constantijn@gohike.nl>
2023-05-31 22:49:05 +0200

    updated env

--- backend/config/environments/production.php
+++ backend/config/environments/production.php
@@ -13,5 +13,5 @@ Config::define('DB_PASSWORD', getenv('DATABASE_WORDPRESS_PASSWORD'));
 Config::define('DB_HOST', getenv('DATABASE_WORDPRESS_HOST'));
 
 Config::define('WP_HOME', 'https://pvb.forest.imaretarded.dev');
-Config::define('WP_BASEURL', 'https://https://backend.pvb.forest.imaretarded.dev');
-Config::define('WP_SITEURL', 'https://https://backend.pvb.forest.imaretarded.dev/wp');
+Config::define('WP_BASEURL', 'https://backend.pvb.forest.imaretarded.dev');
+Config::define('WP_SITEURL', 'https://backend.pvb.forest.imaretarded.dev/wp');
